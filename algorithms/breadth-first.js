eb1b062a4bb45e12af16c6691b1e17cfc8671180
dhruvpattem <dhruv.pattem@gmail.com>
2021-03-15 22:07:09 +0000

    first commit

---
+++ algorithms/breadth-first.js
@@ -0,0 +1,60 @@
+class BreadthFirst {
+
+    static running = false
+    static timer = ms => new Promise(res => setTimeout(res, ms))
+
+    static async bfs(start, graph, delay) {
+        this.running = !this.running
+
+        var queue = [start]
+        var visited = { start: true }
+        var path = {}
+
+        while (queue.length > 0) {
+            await this.timer(delay)
+            if (!this.running) {
+                draw(graph)
+                this.running = true
+                return
+            }
+
+            var node = queue.shift()
+            if (node != start) block(node[0], node[1], SCAN)
+
+            for (var idx = 0; idx < DELTAS.length; idx++) {
+                var [dx, dy] = DELTAS[idx]
+                var n = [node[0] + dx, node[1] + dy]
+
+                if (0 <= n[0] && n[0] < SIZE_X &&
+                    0 <= n[1] && n[1] < SIZE_Y) {
+                    var type = graph[n[1]][n[0]]
+                    if (!(n.toString() in visited)) {
+                        path[n] = node
+                        if (type == EMPTY) {
+                            visited[n] = true
+                            queue.push(n)
+                        } else if (type == END) {
+                            this.running = false
+                            return [path, n]
+                        }
+                    }
+                }
+            }
+        }
+        this.running = false
+    }
+
+    static async shortest(graph, delay = 5) {
+        var start = [S.x, S.y]
+        var ret = await this.bfs(start, graph, delay)
+        if (ret) {
+            var [path, node] = ret
+            while (true) {
+                await this.timer(delay * 2)
+                var node = path[node]
+                if (node[0] == start[0] && node[1] == start[1]) break
+                block(node[0], node[1], PATH)
+            }
+        }
+    }
+}
---
+++ index.js
@@ -0,0 +1,93 @@
+canvas = document.getElementById('canvas')
+ctx = canvas.getContext('2d')
+
+EMPTY = 0, WALL = 1, START = 2, END = 3
+
+COLORS = {
+    [EMPTY]: '#e0e0e0',
+    [START]: 'blue',
+    [END]: 'green',
+    [WALL]: 'black',
+}
+
+PATH = 'red'
+SCAN = 'lightblue'
+
+SIZE_X = 30
+SIZE_Y = 20
+
+S = { x: 5, y: 10 }
+E = { x: 24, y: 10 }
+
+UNIT = 30
+GAP = 1
+
+SCALE_X = SIZE_X * UNIT
+SCALE_Y = SIZE_Y * UNIT
+
+DELTAS = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+
+setup()
+
+function setup() {
+    canvas.width = SCALE_X
+    canvas.height = SCALE_Y
+    var graph = Array(SIZE_Y).fill().map(() => Array(SIZE_X).fill(EMPTY))
+    graph[S.y][S.x] = START
+    graph[E.y][E.x] = END
+    draw(graph)
+    listener(graph)
+
+    BreadthFirst.shortest(graph)
+}
+
+function listener(graph) {
+    var down = false
+    var erase = false
+    var x, y;
+    document.addEventListener('mouseup', () => down = false)
+    document.addEventListener('mousedown', e => {
+        down = true
+        x = mouse(e).x
+        y = mouse(e).y
+        erase = graph[y][x] == WALL
+        edit(graph, x, y, erase ? EMPTY : WALL)
+    })
+    document.addEventListener('mousemove', e => {
+        if (x != null && y != null && down) {
+            if (mouse(e).x != x || mouse(e).y != y) {
+                x = mouse(e).x
+                y = mouse(e).y
+                edit(graph, x, y, erase ? EMPTY : WALL)
+            }
+        }
+    })
+}
+
+function edit(graph, x, y, type) {
+    if (graph[y][x] == START || graph[y][x] == END) return
+    graph[y][x] = type
+    draw(graph)
+
+    BreadthFirst.shortest(graph)
+}
+
+function mouse(e) {
+    rect = canvas.getBoundingClientRect()
+    x = Math.floor((e.clientX - rect.left) / UNIT)
+    y = Math.floor((e.clientY - rect.top) / UNIT)
+    return { x: x, y: y }
+}
+
+function draw(graph) {
+    for (var y = 0; y < SIZE_Y; y++) {
+        for (var x = 0; x < SIZE_X; x++) {
+            block(x, y, COLORS[graph[y][x]])
+        }
+    }
+}
+
+function block(x, y, color) {
+    ctx.fillStyle = color
+    ctx.fillRect(x * UNIT, y * UNIT, UNIT - GAP, UNIT - GAP)
+}
